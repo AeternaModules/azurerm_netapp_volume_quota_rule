29f1f79d128615486266d90074cd99dee050a17f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "netapp_volume_quota_rules" {
-  description = "All netapp_volume_quota_rule resources"
-  value       = azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules
-}
 output "netapp_volume_quota_rules_location" {
-  description = "List of location values across all netapp_volume_quota_rules"
-  value       = [for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : v.location]
+  description = "Map of location values across all netapp_volume_quota_rules, keyed the same as var.netapp_volume_quota_rules"
+  value       = { for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : k => v.location }
 }
 output "netapp_volume_quota_rules_name" {
-  description = "List of name values across all netapp_volume_quota_rules"
-  value       = [for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : v.name]
+  description = "Map of name values across all netapp_volume_quota_rules, keyed the same as var.netapp_volume_quota_rules"
+  value       = { for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : k => v.name }
 }
 output "netapp_volume_quota_rules_quota_size_in_kib" {
-  description = "List of quota_size_in_kib values across all netapp_volume_quota_rules"
-  value       = [for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : v.quota_size_in_kib]
+  description = "Map of quota_size_in_kib values across all netapp_volume_quota_rules, keyed the same as var.netapp_volume_quota_rules"
+  value       = { for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : k => v.quota_size_in_kib }
 }
 output "netapp_volume_quota_rules_quota_target" {
-  description = "List of quota_target values across all netapp_volume_quota_rules"
-  value       = [for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : v.quota_target]
+  description = "Map of quota_target values across all netapp_volume_quota_rules, keyed the same as var.netapp_volume_quota_rules"
+  value       = { for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : k => v.quota_target }
 }
 output "netapp_volume_quota_rules_quota_type" {
-  description = "List of quota_type values across all netapp_volume_quota_rules"
-  value       = [for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : v.quota_type]
+  description = "Map of quota_type values across all netapp_volume_quota_rules, keyed the same as var.netapp_volume_quota_rules"
+  value       = { for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : k => v.quota_type }
 }
 output "netapp_volume_quota_rules_volume_id" {
-  description = "List of volume_id values across all netapp_volume_quota_rules"
-  value       = [for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : v.volume_id]
+  description = "Map of volume_id values across all netapp_volume_quota_rules, keyed the same as var.netapp_volume_quota_rules"
+  value       = { for k, v in azurerm_netapp_volume_quota_rule.netapp_volume_quota_rules : k => v.volume_id }
 }
 
